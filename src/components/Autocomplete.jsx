1db0ad3287e492c71af03d7b3d28d3cfd2011309
ajinkyaashok <ajinkyaashokghate@gmail.com>
2026-08-26 13:28:32 +0530

    feat: Update Autocomplete component to fetch results from API and display loading/error states

--- src/components/Autocomplete.jsx
+++ src/components/Autocomplete.jsx
@@ -1,26 +1,47 @@
 import React, { useEffect, useState } from "react";
 
-export default function Autocomplete({ options }) {
+export default function Autocomplete() {
   const [word, setWord] = useState("");
   const [debounced, setDebounced] = useState("");
+  const [results, setResults] = useState([]);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     const timer = setTimeout(() => {
-        setDebounced(word)
+      setDebounced(word);
     }, 1000);
     return () => clearTimeout(timer);
   }, [word]);
 
+  useEffect(() => {
+    async function fetchResults() {
+      try {
+        setLoading(true);
+        setError(null);
+        const res = await fetch("https://jsonplaceholder.typicode.com/users");
+        const data = await res.json();
+        setResults(data);
+      } catch (err) {
+        setError(err);
+      } finally {
+        setLoading(false);
+      }
+    }
+    fetchResults();
+  }, [debounced]);
+
   const filteredOptions =
     debounced.trim() === ""
       ? []
-      : options.filter((item) =>
-          item.toLowerCase().includes(debounced.toLowerCase()),
+      : results.filter((item) =>
+          item.name.toLowerCase().includes(debounced.toLowerCase()),
         );
 
   return (
     <div className="flex mx-auto flex-col justify-center items-center h-screen">
       <h1>Autocomplete</h1>
+
       <input
         onChange={(e) => setWord(e.target.value)}
         type="text"
@@ -29,11 +50,15 @@ export default function Autocomplete({ options }) {
         value={word}
         className="bg-amber-200 rounded-2xl"
       />
-
+      {loading && <h1>Loading...</h1>}
+      {error && <h1>Error</h1>}
       {filteredOptions.map((item) => (
-        <div onClick={() => setWord(item)}>{item}</div>
+        <div key={item.id} onClick={() => setWord(item.name)}>
+          {item.name}
+        </div>
       ))}
       {word && filteredOptions.length === 0 && <div>No results found</div>}
     </div>
   );
 }
+
